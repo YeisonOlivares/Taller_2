0b9005b9d990ad339a10cb019e958b15b6cb71c9
Rodrigo Dominguez <rodrigo.dom@outlook.cl>
2017-10-27 16:29:06 -0300

    Add files via upload

--- src/ListaPrestamo.java
+++ src/ListaPrestamo.java
@@ -5,15 +5,18 @@
  */
 
 /**
- *
- * @author Wolf Paradise
+ * Lista para almacenar los prestamos registrados
+ * @author Yeison Olivares, Rodrigo Dominguez
  */
 public class ListaPrestamo {
     
     private Prestamo[] listaPrestamo;
     private int maxPrestamo;
     private int cantElementos;
-    
+    /**
+     * Constructor de la lista prestamos
+     * @param maxPrestamo asigna el valor inicial de 0 a la cantidad de prestamos ingresados
+     */
     public ListaPrestamo(int maxPrestamo){
         
         this.maxPrestamo = maxPrestamo;
@@ -21,14 +24,16 @@ public class ListaPrestamo {
         this.listaPrestamo = new Prestamo[maxPrestamo];
         
     }
-    
+    /**
+     * Metodo para agregar un nuevo prestamo a la lista
+     * @param prestamo se ingresa una variable del tipo prestamo con sus atributos
+     * @return retorna un boleano verdadero si el prestamo de agrego con exito, de lo contrario un false
+     */
     public boolean agregarPrestamo(Prestamo prestamo){
         
         if(this.cantElementos >= this.maxPrestamo){
             return false;
-        }
-        
-        else{
+        }else{
             
             this.listaPrestamo[cantElementos] = prestamo;
             cantElementos++;
@@ -36,15 +41,27 @@ public class ListaPrestamo {
             
         }
     }
-    
+    /**
+     * Metodo para obtener un prestamo de la lista ingresando una posicion
+     * @param pos se ingresa la posicion a buscar en la lista
+     * @return retorna el prestamo en dicha posicion
+     */
     public Prestamo getPrestamo(int pos){
         return listaPrestamo[pos];
     }
-    
+    /**
+     * Metodo para obtener el total de prestamos ingresados a la lista
+     * @return retorna el total de prestamos ingresados a la lista
+     */
     public int getTotalPrestamos(){
         return cantElementos;
     }
     
+    /**
+     * Metodo para modificar un prestamo de la lista de prestamos
+     * @param pos se ingresa la posicion del prestamo en la lista
+     * @param prestamo se ingresa el prestamo a modificar
+     */
     public void setPrestamo(int pos, Prestamo prestamo){
         listaPrestamo[pos] = prestamo;
     }
