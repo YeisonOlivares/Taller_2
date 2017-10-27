085f2e8eef18c14f1c99608a58219aa04d06a329
Rodrigo Dominguez <rodrigo.dom@outlook.cl>
2017-10-27 15:51:50 -0300

    Add files via upload

--- src/Cliente.java
+++ src/Cliente.java
@@ -5,8 +5,9 @@
  */
 
 /**
- *
- * @author Wolf Paradise
+ * Clase Cliente
+ * @author Yeison Olivares, Rodrigo Dominguez
+ * 
  */
 public class Cliente {
     
@@ -17,6 +18,15 @@ public class Cliente {
     private String email;
     private ListaPrestamo listaPrestamo;
     
+    /**
+     * Constructor de la clase Cliente
+     * @param rut Asigna un rut a cada cliente
+     * @param nombre Asigna un nombre a cada cliente
+     * @param apellido Asigna un apellido a cada cliente
+     * @param celular Asigna un numero de celular a cada cliente
+     * @param email Asigna un correo electronico a cada cliente
+     * @param listaPrestamo El cliente esta asociado a una lista de prestamos
+     */
     public Cliente(String rut, String nombre, String apellido, int celular, String email, ListaPrestamo listaPrestamo){
     this.rut = rut;
     this.nombre = nombre;
@@ -26,19 +36,31 @@ public class Cliente {
     this.listaPrestamo = null;
     
     }
-    
+    /**
+     * Obtener el rut
+     * @return retorna el rut cosultado
+     */
     public String getRut(){
         return this.rut;
     }
-    
+    /**
+     * Obtener el nombre
+     * @return retorna el nombre consultado
+     */
     public String getNombre(){
         return this.nombre;
     }
-    
+    /**
+     * Obtener el apellido
+     * @return retorna el apellido consultado
+     */
     public String getApellido(){
         return this.apellido;
     }
-    
+    /**
+     * Obtener el numero de celular
+     * @return retorna el numero de celular consultado
+     */
     public int getCelular(){
         return this.celular;
     }
